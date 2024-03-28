0de83601830185368aed4b1975c4d942cf6ad182
Christian Redfern <credfernbus@gmail.com>
2024-03-28 16:29:00 +0000

    Prepopulation works, making script for it

--- src/main/resources/testData/PopulateDynamo.sh
+++ src/main/resources/testData/PopulateDynamo.sh
@@ -4,4 +4,6 @@
 # - Default region name [None]: eu-west-2
 # - AWS Access Key ID [None]: <Leave Blank>
 
+aws dynamodb create-table --endpoint-url http://localhost:8000 --table-name User --attribute-definitions AttributeName=Id,AttributeType=S --key-schema AttributeName=Id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
 aws dynamodb batch-write-item --endpoint-url http://localhost:8000 --request-items file://ExampleUser.json
